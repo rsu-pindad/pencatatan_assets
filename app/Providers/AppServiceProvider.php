d4bbd63987122fc30c16a1406811647a7658342a
rsu-pindad <sautomated01@gmail.com>
2024-08-27 15:19:40 +0700

    change locale to ID

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Number;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        config(['app.locale' => 'id']);
+        Number::useLocale('id');
+        Carbon::setLocale('id');
     }
 }
