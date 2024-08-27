adc09de9f28aaf0ea27b6a65bcd1694de82c11b8
rsu-pindad <sautomated01@gmail.com>
2024-08-27 08:04:09 +0700

    add flatpickr css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
 import './bootstrap';
 import '../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
 import '../../vendor/power-components/livewire-powergrid/dist/powergrid';
+import '../../node_modules/flatpickr/dist/flatpickr.css';
+// import '../../node_modules/flatpickr/src/style/flatpickr.styl';
