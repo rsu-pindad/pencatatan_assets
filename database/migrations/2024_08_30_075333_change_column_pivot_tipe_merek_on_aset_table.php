bf3eacff95adb5b063298a0b179b1b19000cd198
rsu-pindad <sautomated01@gmail.com>
2024-08-30 08:02:21 +0700

    change column type

---
+++ database/migrations/2024_08_30_075333_change_column_pivot_tipe_merek_on_aset_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('aset', function (Blueprint $table) {
+            $table->dropForeign('aset_pivot_tipe_merek_id_foreign');
+            $table->unsignedBigInteger('pivot_tipe_merek_id')->nullable()->change();
+            $table
+                ->foreign('pivot_tipe_merek_id')
+                ->references('id')
+                ->on('pivot_tipe_merek')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('aset', function (Blueprint $table) {
+            $table->dropForeign('aset_pivot_tipe_merek_id_foreign');
+            $table->unsignedBigInteger('pivot_tipe_merek_id')->change();
+            $table
+                ->foreign('pivot_tipe_merek_id')
+                ->references('id')
+                ->on('pivot_tipe_merek')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+};
