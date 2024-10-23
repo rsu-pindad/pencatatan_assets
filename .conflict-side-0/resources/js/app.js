27fbab3f262808efab76bafe9ee9cc32d78228d9
rsu-pindad <sautomated01@gmail.com>
2024-10-23 09:08:22 +0700

    Merge branch 'gitbutler/workspace' of https://github.com/rsu-pindad/pencatatan_assets into gitbutler/workspace

--- .conflict-side-0/resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import './bootstrap';
-import '../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
-import '../../vendor/power-components/livewire-powergrid/dist/powergrid';
-import '../../node_modules/flatpickr/dist/flatpickr.css';
-// import '../../node_modules/flatpickr/src/style/flatpickr.styl';
--- .conflict-side-1/resources/views/components/power/spinner-loading.blade.php
+++
@@ -1,7 +0,0 @@
-<div
-     class="flex items-center justify-center rounded-lg border border-gray-200 bg-gray-50 py-12 dark:border-gray-700 dark:bg-gray-800">
-  <div
-       class="animate-pulse rounded-full bg-blue-200 px-3 py-1 text-center text-xs font-medium leading-none text-lime-800 dark:bg-lime-900 dark:text-lime-200">
-    tunggu...
-  </div>
-</div>
