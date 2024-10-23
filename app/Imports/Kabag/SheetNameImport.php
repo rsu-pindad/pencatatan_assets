dd09088736b840d05b620f9a362c75337638feb7
rsu-pindad <sautomated01@gmail.com>
2024-10-23 08:18:23 +0700

    fix import limit

--- app/Imports/Kabag/SheetNameImport.php
+++ app/Imports/Kabag/SheetNameImport.php
@@ -21,13 +21,14 @@ class SheetNameImport implements WithMultipleSheets
     public function conditionalSheets(): array
     {
         return [
-            'Unit'      => new UnitImport($this->awal, $this->akhir),
-            'Tipe'      => new TipeImport($this->awal, $this->akhir),
-            'Merek'     => new MerekImport($this->awal, $this->akhir),
-            'TipeMerek' => new TipeMerekImport($this->awal, $this->akhir),
-            'Vendor'    => new VendorImport($this->awal, $this->akhir),
-            'Satuan'    => new SatuanImport($this->awal, $this->akhir),
-            'Kode'      => new KodeImport($this->awal, $this->akhir),
+            'Unit'          => new UnitImport($this->awal, $this->akhir),
+            'Tipe'          => new TipeImport($this->awal, $this->akhir),
+            'Merek'         => new MerekImport($this->awal, $this->akhir),
+            'TipeMerek'     => new TipeMerekImport($this->awal, $this->akhir),
+            'Vendor'        => new VendorImport($this->awal, $this->akhir),
+            'Satuan'        => new SatuanImport($this->awal, $this->akhir),
+            'Kode'          => new KodeImport($this->awal, $this->akhir),
+            'DATA ASET TRF' => new AsetImport($this->awal, $this->akhir),
         ];
     }
 }
